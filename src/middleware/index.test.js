c6754f03f91759d9a9daf10dcfebd6883b7cb487
mStybowski <mstybowski@gmail.com>
2022-05-30 18:36:43 +0200

    Changed tests to match new endpoints

--- src/middleware/index.test.js
+++ src/middleware/index.test.js
@@ -4,39 +4,39 @@ const { server } = require("../server");
 
 describe("Get endpoint", () => {
   it("should fail getting movie when no value for GET queries provided", async () => {
-    await request(server).get("/get?duration=").expect(400);
-    await request(server).get("/get?genres=").expect(400);
-    await request(server).get("/get?duration=&genres=").expect(400);
+    await request(server).get("/movies?duration=").expect(400);
+    await request(server).get("/movies?genres=").expect(400);
+    await request(server).get("/movies?duration=&genres=").expect(400);
   });
 
   it("should not fail getting movie when GET queries are correct", async () => {
     await request(server)
-      .get("/get?duration=120")
+      .get("/movies?duration=120")
       .expect("Content-Type", /json/)
       .expect(200);
     await request(server)
-      .get("/get?genres=Action,Crime")
+      .get("/movies?genres=Action,Crime")
       .expect("Content-Type", /json/)
       .expect(200);
     await request(server)
-      .get("/get")
+      .get("/movies")
       .expect("Content-Type", /json/)
       .expect(200);
   });
 
   it("should fail adding movie with invalid or incomplete POST body", async () => {
     await request(server)
-      .post("/add")
+      .post("/movies")
       .send({})
       .expect("Content-Type", /json/)
       .expect(400);
     await request(server)
-      .post("/add")
+      .post("/movies")
       .send({ title: "someTitle" })
       .expect("Content-Type", /json/)
       .expect(400);
     await request(server)
-      .post("/add")
+      .post("/movies")
       .send({ title: "someTitle", genres: ["Action"] })
       .expect("Content-Type", /json/)
       .expect(400);
